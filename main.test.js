1f6fb7d703cafb38bf872509dd7c5792bb3d26bf
DenverDawgs18 <pautomas55@gmail.com>
2024-02-14 05:50:27 -0500

    Psuedocode for tests has been written

---
+++ main.test.js
@@ -0,0 +1,24 @@
+// Test one: hit function works as expected (updates number of hits on a given ship)
+
+
+// Test two: ocean function works as expected (this ensures length is working) and ensures sunk is true
+
+// Those are the only tests for the ship class (using classes because they look more like python which is my fav lang)
+
+
+
+// Test three p o:  make sure gameboards are placing ships at a coordinate
+// This is going to make gameboards implemented as a 2d array and at those coordinates it will be replaced by something
+// that says ship X 
+
+
+// Test four: gameboards cannot place ships if there is a ship there 
+
+
+// Test five: recieveAttack works as expected - takes a pair of coords, checks if a ship is hit. This is the hit case
+
+// Test fiveB: recieveAttack works when the attack misses
+
+// Test 6: Gameboard properly determines if all of its ships are in the ocean
+
+
